9fcded1af6388b33a8646e8941073fec966e633b
tekuralasony <sonytekurala202@gmail.com>
2026-01-02 18:12:34 +0530

    Added OOP basics in Python

---
+++ opp_basics.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        return f"Hi, my name is {self.name}"
+
+p = Person("Sony", 22)
+print(p.greet())
